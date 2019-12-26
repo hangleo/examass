960436d268763b62115967f13ba656a44a7da407
Developer <developer@ouhk.edu.hk>
2019-12-26 15:02:57 +0800

    Add existing file

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello, world!');    
+})
+
+const server = app.listen(process.env.PORT || 8099, () => {
+    const port = server.address().port;
+    console.log(`Server is listening at port ${port}`); 
+});
